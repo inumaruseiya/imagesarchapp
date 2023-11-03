4a6c4ce4cca2c9e28a0e52dff4b80911774e55fb
inumaruseiya <s.inumaru2023@gmail.com>
2023-11-03 12:35:35 +0900

    検索機能、UIデザイン

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,9 @@ class PixabayPage extends StatefulWidget {
 
 class _PixabayPageState extends State<PixabayPage> {
   List imageList = [];
-  Future<void> fetchImages() async {
+  Future<void> fetchImages(String text) async {
     Response response = await Dio().get(
-        'https://pixabay.com/api/?key=16114263-a90518396670e0f064ecc5034&q=yellow+flowers&image_type=photo&pretty=true');
+        'https://pixabay.com/api/?key=16114263-a90518396670e0f064ecc5034&q=$text&image_type=photo&pretty=true');
     imageList = response.data['hits'];
     setState(() {});
   }
@@ -36,12 +36,24 @@ class _PixabayPageState extends State<PixabayPage> {
   @override
   void initState() {
     super.initState();
-    fetchImages();
+    fetchImages('花');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) {
+            print(text);
+            fetchImages(text);
+          },
+        ),
+      ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
@@ -49,7 +61,34 @@ class _PixabayPageState extends State<PixabayPage> {
         itemCount: imageList.length,
         itemBuilder: (context, index) {
           Map<String, dynamic> image = imageList[index];
-          return Image.network(image['previewURL']);
+          return Stack(
+
+            fit: StackFit.expand,
+            children: [
+              Image.network(
+                image['previewURL'],
+
+                fit: BoxFit.cover,
+              ),
+
+              Align(
+                alignment:  Alignment.bottomRight,
+                child: Container(
+                  color:  Colors.white,
+                  child:  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Icon(
+                        Icons.thumb_up_alt_outlined,
+                        size: 14,
+                      ),
+                      Text('${image['likes']}'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
         },
       ),
     );
